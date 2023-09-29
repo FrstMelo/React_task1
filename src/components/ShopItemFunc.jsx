85dbe083347de77087b74548d93b1a4fbb6e8318
Vitaliy Bushai <ninegatesopen@gmail.com>
2023-09-29 18:24:26 +0300

    First commit

---
+++ src/components/ShopItemFunc.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import '../main.css';
+
+let ShopItemFunc = (props) => {
+    
+  function formatPrice(price) {
+    return price.toFixed(2);
+  }
+
+      return (
+        <div className="main-content">
+        <h2>{props.brand}</h2>
+        <h1>{props.title}</h1>
+        <h3>{props.description}</h3>
+        <div className="description">
+            {props.descriptionFull}
+        </div>
+        <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+        <div className="divider"></div>
+        <div className="purchase-info">
+        <div className="price"><span>{props.currency}</span>{formatPrice(props.price)}</div>
+        <button>Добавить в корзину</button>
+        </div>
+        </div>
+      )
+}
+
+export default ShopItemFunc;
+    
